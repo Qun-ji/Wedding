990b641db01d5c9ea0642d939c08d699131aafeb
Qun-ji <1409373145@qq.com>
2025-08-30 22:55:44 +0800

    6

--- src/database.js
+++ src/database.js
@@ -1,5 +1,7 @@
 import { neon } from '@neondatabase/serverless'
 
+console.log('数据库URL配置:', import.meta.env.VITE_DATABASE_URL ? '已配置' : '未配置')
+
 const sql = neon(import.meta.env.VITE_DATABASE_URL)
 
 export default sql
